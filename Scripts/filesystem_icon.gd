c8dfb525f89994f01f015f869f2a0dd61329751a
Gabriele <yugo.amaril@gmail.com>
2022-06-16 18:29:53 +0200

    some logic

--- Scripts/filesystem_icon.gd
+++ Scripts/filesystem_icon.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends ToolButton
 
 
 # Declare member variables here. Examples:
@@ -8,12 +8,13 @@ extends Sprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_process_input(true)
-
-func _input(ev):
-	if ev.type==InputEvent.MOUSE_BUTTON and ev.is_pressed() and ev.button_index==1 and ev.doubleclick:
-		get_tree().change_scene("res://Scenes/Files.tscn")
+	pass
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_filesystem_pressed():
+	get_tree().change_scene("res://Scenes/Files.tscn")
